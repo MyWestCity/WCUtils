b17603e9cbd83251deb696d0bf6df42904317d2a
xq <>
2020-07-07 17:37:48 +0800

    修改NSString

---
+++ GobangGame/GobangGame/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  GobangGame
+//
+//  Created by xq on 2020/4/8.
+//  Copyright © 2020 yzm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ GobangGame/GobangGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GobangGame
+//
+//  Created by xq on 2020/4/8.
+//  Copyright © 2020 yzm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
